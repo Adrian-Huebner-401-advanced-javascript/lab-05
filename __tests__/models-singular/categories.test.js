18f7514cbfe7077faf71cd494d15a5d4427cba6d
adrianhuebner <adrianh92@gmail.com>
2019-10-21 10:36:45 -0700

    code from review added to files

--- __tests__/models-singular/categories.test.js
+++ __tests__/models-singular/categories.test.js
@@ -8,25 +8,17 @@ describe('Categories Model (Singular)', () => {
   // How will you handle both the happy path and edge cases in these tests?
 
   it('can create() a new category', () => {
-    let newCat = { name: 'Clothing', description: 'All the clothes that we sell'};
-    return categories.create(newCat)
-      .then(record => {
-        Object.keys(newCat).forEach(key => {
-          expect(record[key]).toEqual(newCat[key]);
-        });
+    let newCategory = {name: 'Test', description: 'This is a test Category'};
+    categories.create(newCategory)
+      .then((result) => {
+        expect(result.name).toEqual('Test');
       });
   });
 
   it('can get() a category', () => {
-    let newCat = { name: 'Clothing', description: 'All the clothes that we sell'};
-    return categories.create(newCat)
-      .then(record => {
-        return categories.get(record._id)
-          .then(catergories => {
-            Object.keys(newCat).forEach(key => {
-              expect(categories[0][key]).toEqual(categories[key]);
-            });
-          });
+    categories.get()
+      .then(results => {
+        expect(results.length);
       });
   });
 
--- models-singular/categories.js
+++ models-singular/categories.js
@@ -2,7 +2,7 @@
 
 // Where is our schema defined?
 // How do we get it in here so we can run methods on it?
-const catSchema = require('./categories-schema');
+const CatSchema = require('./categories-schema');
 
 
 class Categories {
@@ -12,31 +12,27 @@ class Categories {
 
   get(_id) {
     // Call the appropriate mongoose method to get
-    // one or more records
-    // If 1, return it as a plain object
-    // If 2, return it as an object like this:
-    // { count: ##, results: [{}, {}] }
     if(_id){
-      return catSchema.findOne({_id});
+      return CatSchema.findOne({_id});
     } else {
-      return catSchema.find({});
+      return CatSchema.find({});
     }
   }
 
   create(record) {
     // Call the appropriate mongoose method to create a new record
-    let newRecord = new catSchema(record);
+    let newRecord = new CatSchema(record);
     return newRecord.save();
   }
 
   update(_id, record) {
     // Call the appropriate mongoose method to update a record
-    return catSchema.findByIdAndUpdate(_id, record, {new : true});
+    return CatSchema.findByIdAndUpdate(_id, record, {new : true});
   }
 
   delete(_id) {
     // Call the appropriate mongoose method to delete a record
-    return catSchema.findByIdAndDelete(_id);
+    return CatSchema.findByIdAndDelete(_id);
   }
 
 }
