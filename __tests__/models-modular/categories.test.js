58a2a7c2dc2a773bffef675a66480ec16660e472
adrianhuebner <adrianh92@gmail.com>
2019-10-21 10:45:58 -0700

    more added to files

--- __tests__/models-modular/categories.test.js
+++ __tests__/models-modular/categories.test.js
@@ -1,13 +1,19 @@
-//const Categories = require('../../models-modular/categories/categories.js');
-//let categories = new Categories();
+const Categories = require('../../models-modular/categories/categories.js');
+let categories = new Categories();
 
-//const supergoose = require('../supergoose.js');
+const supergoose = require('../supergoose.js');
 
 describe('Categories Model (Modular)', () => {
 
   // How will you handle both the happy path and edge cases in these tests?
 
   it('can create() a new category', () => {
+    let newCategory = { name: 'Test', description: 'Test Subject'};
+    categories.create(newCategory)
+      .then(result => {
+        expect(result.name).toBe('Test');
+        done();
+      });
   });
 
   it('can get() a category', () => {
