c1466c4eab2676a26c25b0469f661defc788ae9a
adrianhuebner <adrianh92@gmail.com>
2019-10-20 22:30:21 -0700

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# lab-05
+# LAB - 05
+
+## Data Modeling With NoSQL Databases
+
+### Author: Adrian Huebner
+
+### Links and Resources
+* [submission PR](http://xyz.com)
+* [travis](https://www.travis-ci.com/Adrian-Huebner-401-advanced-javascript/lab-05)
+* [front-end](https://adrianhuebner-lab05.herokuapp.com/)
+
+### Modules
+#### `categories.js` (both in models-singular file and models-modular file)
+#### `products.js` (both in models-singular file and models-modular file)
+##### Exported Values and Methods
+
+###### `foo(thing) -> string`
+Usage Notes or examples
+
+#### Running the app
+* `npm start`
+  
+#### Tests
+* How do you run tests?
+`npm run test` and `npm run lint`
+
+
+#### UML
+![lab5UML](./assests/Lab5.png)
--- index.js
+++ index.js
@@ -3,14 +3,31 @@
 const mongoose = require('mongoose');
 
 // Require your model
+const SingularCategories = require('./models-singular/categories');
 
 // Mongoose Server URI
 const MONGOOSE_URI = 'mongodb://localhost:27017/class05';
 
 // Connect
-mongoose.connect(...);
+mongoose.connect(MONGOOSE_URI, { useNewUrlParser: true });
 
 // Do some work
+let singularCategories = new SingularCategories;
+
+const doData = async() => {
+  let sampleCategories = {
+    name: 'Clothing',
+    description: 'All the clothes we sell',
+  };
+
+  let newCategories = await singularCategories.create(sampleCategories);
+  console.log('Category Created', newCategories);
+
+  let allCategories = await singularCategories.get();
+  console.log('All Categories', allCategories);
+};
 
 // Disconnect
 mongoose.disconnect();
+
+doData();
